72aa5dc75deb613a172be19092cf0dca4c34792a
George Farro <george@scandy.co>
2020-04-04 16:48:17 -0500

    random logy

--- src/Logo.js
+++ src/Logo.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export class Logo extends React.Component {
+  getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive
+  }
   render() {
     return (
-      <img alt='POND Coffee logomark' className='logo' src={`/svg/7.svg`} />
+      <img
+        alt='POND Coffee logomark'
+        className='logo'
+        src={`/svg/${this.getRandomIntInclusive(1, 7)}.svg`}
+      />
     )
   }
 }
